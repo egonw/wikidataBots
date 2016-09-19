b612b47273a5a67b4cb6adb82394da648c51729a
Andra Waagmeester <andra@micelio.be>
2016-09-19 12:21:47 +0200

    human genes to automation mode

---
+++ automated_bots/genes/mammals/ProteinBoxBotKnowledge.py
@@ -0,0 +1,113 @@
+
+#references 
+## databases
+reference2entrezGene = 'Q1345229'  
+reference2ensembl = 'Q1344256'
+
+## species 
+humanSpeciesPage = 'Q15978631'
+mouseSpeciesPage = 'Q83310'
+genePage = 'Q7187'
+
+ # Adding knowledge
+ # mus_musculus Chromosomes
+chromosomes = dict()
+chromosomes["mus_musculus"] = dict()
+chromosomes["mus_musculus"]['1'] = "Q14864323"
+chromosomes["mus_musculus"]['2'] = "Q15304650"
+chromosomes["mus_musculus"]['3'] = "Q15304652"
+chromosomes["mus_musculus"]['4'] = "Q15304654"
+chromosomes["mus_musculus"]['5'] = "Q15304656"
+chromosomes["mus_musculus"]['6'] = "Q15305576"
+chromosomes["mus_musculus"]['7'] = "Q15305579"
+chromosomes["mus_musculus"]['8'] = "Q15305584"
+chromosomes["mus_musculus"]['9'] = "Q15305586"
+chromosomes["mus_musculus"]['10'] = "Q15305590"
+chromosomes["mus_musculus"]['11'] = "Q15305594"
+chromosomes["mus_musculus"]['12'] = "Q15305596"
+chromosomes["mus_musculus"]['13'] = "Q15305599"
+chromosomes["mus_musculus"]['14'] = "Q15305602"
+chromosomes["mus_musculus"]['15'] = "Q15305608"
+chromosomes["mus_musculus"]['16'] = "Q15305612"
+chromosomes["mus_musculus"]['17'] = "Q15305616"
+chromosomes["mus_musculus"]['18'] = "Q15305619"
+chromosomes["mus_musculus"]['19'] = "Q15305621"
+chromosomes["mus_musculus"]['X'] = "Q15305624"
+chromosomes["mus_musculus"]['Y'] = "Q15305628" 
+
+# chromosomes["homo_sapiens"]
+#Chromosomes dict
+chromosomes["homo_sapiens"] = dict()
+chromosomes["homo_sapiens"]['1'] = "Q430258"
+chromosomes["homo_sapiens"]['2'] = "Q638893"
+chromosomes["homo_sapiens"]['3'] = "Q668633"
+chromosomes["homo_sapiens"]['4'] = "Q836605"
+chromosomes["homo_sapiens"]['5'] = "Q840741"
+chromosomes["homo_sapiens"]['6'] = "Q540857"
+chromosomes["homo_sapiens"]['7'] = "Q657319"
+chromosomes["homo_sapiens"]['8'] = "Q572848"
+chromosomes["homo_sapiens"]['9'] = "Q840604"
+chromosomes["homo_sapiens"]['10'] = "Q840737"
+chromosomes["homo_sapiens"]['11'] = "Q847096"
+chromosomes["homo_sapiens"]['12'] = "Q847102"
+chromosomes["homo_sapiens"]['13'] = "Q840734"
+chromosomes["homo_sapiens"]['14'] = "Q138955"
+chromosomes["homo_sapiens"]['15'] = "Q765245"
+chromosomes["homo_sapiens"]['16'] = "Q742870"
+chromosomes["homo_sapiens"]['17'] = "Q220677"
+chromosomes["homo_sapiens"]['18'] = "Q780468"
+chromosomes["homo_sapiens"]['19'] = "Q510786"
+chromosomes["homo_sapiens"]['20'] = "Q666752"
+chromosomes["homo_sapiens"]['21'] = "Q753218"
+chromosomes["homo_sapiens"]['22'] = "Q753805"
+chromosomes["homo_sapiens"]['X'] = "Q61333"
+chromosomes["homo_sapiens"]['Y'] = "Q202771"
+chromosomes["homo_sapiens"]["MT"] = "Q27075"
+
+chromosomes["rattus_norvegicus"] = dict()
+chromosomes["rattus_norvegicus"]['1'] = "Q21596683"
+chromosomes["rattus_norvegicus"]['2'] = "Q21596688"
+chromosomes["rattus_norvegicus"]['3'] = "Q21596692"
+chromosomes["rattus_norvegicus"]['4'] = "Q21596695"
+chromosomes["rattus_norvegicus"]['5'] = "Q21596697"
+chromosomes["rattus_norvegicus"]['6'] = "Q21596698"
+chromosomes["rattus_norvegicus"]['7'] = "Q21596734"
+chromosomes["rattus_norvegicus"]['8'] = "Q21596735"
+chromosomes["rattus_norvegicus"]['9'] = "Q21596736"
+chromosomes["rattus_norvegicus"]['10'] = "Q21596737"
+chromosomes["rattus_norvegicus"]['11'] = "Q21596738"
+chromosomes["rattus_norvegicus"]['12'] = "Q21596739"
+chromosomes["rattus_norvegicus"]['13'] = "Q21596740"
+chromosomes["rattus_norvegicus"]['14'] = "Q21596741"
+chromosomes["rattus_norvegicus"]['15'] = "Q21596742"
+chromosomes["rattus_norvegicus"]['16'] = "Q21596743"
+chromosomes["rattus_norvegicus"]['17'] = "Q21596744"
+chromosomes["rattus_norvegicus"]['18'] = "Q21596745"
+chromosomes["rattus_norvegicus"]['19'] = "Q21596746"
+chromosomes["rattus_norvegicus"]['20'] = "Q21596747"
+chromosomes["rattus_norvegicus"]['X'] = "Q21596753"
+chromosomes["rattus_norvegicus"]['Y'] = "Q21596756"
+
+chromosomes["macaca_nemestrina"] = dict()
+chromosomes["macaca_nemestrina"]['1'] = "Q24451224"
+chromosomes["macaca_nemestrina"]['2'] = "Q24451225"
+chromosomes["macaca_nemestrina"]['3'] = "Q24451227"
+chromosomes["macaca_nemestrina"]['4'] = "Q24451229"
+chromosomes["macaca_nemestrina"]['5'] = "Q24451231"
+chromosomes["macaca_nemestrina"]['6'] = "Q24451233"
+chromosomes["macaca_nemestrina"]['7'] = "Q24451234"
+chromosomes["macaca_nemestrina"]['8'] = "Q24451235"
+chromosomes["macaca_nemestrina"]['9'] = "Q24451236"
+chromosomes["macaca_nemestrina"]['10'] = "Q24451237"
+chromosomes["macaca_nemestrina"]['11'] = "Q24451239"
+chromosomes["macaca_nemestrina"]['12'] = "Q24451240"
+chromosomes["macaca_nemestrina"]['13'] = "Q24451241"
+chromosomes["macaca_nemestrina"]['14'] = "Q24451242"
+chromosomes["macaca_nemestrina"]['15'] = "Q24451243"
+chromosomes["macaca_nemestrina"]['16'] = "Q24451244"
+chromosomes["macaca_nemestrina"]['17'] = "Q24451245"
+chromosomes["macaca_nemestrina"]['18'] = "Q24451246"
+chromosomes["macaca_nemestrina"]['19'] = "Q24451247"
+chromosomes["macaca_nemestrina"]['20'] = "Q24451255"
+chromosomes["macaca_nemestrina"]['Y'] = "Q24451257"
+chromosomes["macaca_nemestrina"]['X'] = "Q24451269"
